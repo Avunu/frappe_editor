79a27e80530facb7c6f0d9f60dd07e93f4ca8636
Kevin Shenk <kevin@avu.nu>
2026-04-23 09:17:22 -0400

    feat: flake for local dev deps

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "SVG Pattern to PDF Generator shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          # Yarn for JavaScript dependencies
+          pkgs.yarn
+        ];
+      };
+    };
+}
